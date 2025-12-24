30d5107bb66b81aa2f729f9706518740188bf21a
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-12-24 16:01:00 +0500

    new changes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     \Modules\Auth\ModuleServiceProvider::class, 
+    \Modules\Billing\ModuleServiceProvider::class,
+    \Modules\AccessControl\ModuleServiceProvider::class,
 ];
