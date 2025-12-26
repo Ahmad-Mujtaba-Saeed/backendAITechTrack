b20a34f2afd39f17cd397b56c992cfd682c888fe
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-12-26 16:55:21 +0500

    new changes

--- modules/User/Models/User.php
+++ modules/User/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable implements Auditable
 
     public function getPlanIdAttribute()
     {
-        return $this->subscription()->where('status', 'active')->first()->type_id ?? null;
+        return $this->subscription()->where('status', 'active')->orWhere('status', 'trialing')->first()->type_id ?? null;
     }
 
     public function subscription()
