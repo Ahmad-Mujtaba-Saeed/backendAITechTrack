6677368527608b255fc741c3016f240223583a6f
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-12-26 15:36:30 +0500

    migrate routes web.p[p

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,57 @@ Route::get('/test-route', function() {
 });
 
 
+
+Route::get('/migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate');
+    return "Migrate Complete!";
+});
+
+Route::get('/static-file-runner-v1', [ResumeController::class, 'staticFileRunner']);
+
+Route::get('/seed', function () {
+    \Illuminate\Support\Facades\Artisan::call('db:seed');
+    return "Seed Complete!";
+});
+Route::get('/optimize-clear', function () {
+    \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+    return "Optimize Clear Complete!";
+});
+
+Route::get('/storage-link', function () {
+    if (file_exists(public_path('storage'))) {
+        return 'Storage link already exists!';
+    }
+    
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return 'Storage link created successfully!';
+});
+
+
+
+
+
+Route::get('/logs', function () {
+    $logFile = storage_path('logs/laravel.log');
+
+    if (!File::exists($logFile)) {
+        return response('Log file not found.', 404);
+    }
+
+    // Read last 500 lines for performance
+    $lines = explode("\n", File::get($logFile));
+    $lastLines = array_slice($lines, -500);
+
+    return Response::make(
+        nl2br(e(implode("\n", $lastLines))),
+        200,
+        ['Content-Type' => 'text/html']
+    );
+});
+
+
+
+
 Route::get('/test-module', function() {
     return response()->json([
         'module_exists' => class_exists(\Modules\Billing\ModuleServiceProvider::class),
