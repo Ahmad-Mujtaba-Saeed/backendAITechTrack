2b3d01da02402ea20f840c659ad6ce5c64427c00
Ahmad Mujtaba <ahmadmujtabap70@gmail.com>
2025-12-31 18:10:36 +0500

    new changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
 Route::prefix('v1')->group(function () {
     
     Route::middleware('auth:sanctum')->group(function () {
